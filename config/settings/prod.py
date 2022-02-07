d81d1ae204bc25c8c5c6cc72c4efa59a980197bd
melody <myeonghee.son@gmail.com>
2022-02-07 16:16:47 +0900

    MOD:settings ALLOWED_HOST

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,5 @@
 from .base import *
-ALLOWED_HOSTS = ['52.78.145.43', 'melodyblog.shop']
+ALLOWED_HOSTS = ['52.78.145.43', 'melodyboard.shop']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG=False
