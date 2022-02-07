22a32552647b822faa5c6bd924a52de48fff1250
melody <myeonghee.son@gmail.com>
2022-02-07 15:16:30 +0900

    ADD:settings pybo logging

--- config/settings/base.py
+++ config/settings/base.py
@@ -195,5 +195,9 @@ LOGGING = {
             'level': 'INFO',
             'propagate': False,
         },
+        'pybo': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
     }
 }
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -3,12 +3,14 @@ from django.core.paginator import Paginator
 from django.db.models      import Q
 
 from ..models import Question
+import logging
+logger = logging.getLogger('pybo')
 
 def index(request):
     """
     pybo 목록 출력
     """
-    3/0  # 강제로 오류발생
+    logger.info("INFO 레벨로 출력")
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
