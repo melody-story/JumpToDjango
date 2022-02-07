ed1af9d328442075a56eb60ec42575c96197dea2
melody <myeonghee.son@gmail.com>
2022-02-07 17:39:07 +0900

    ADD:my_settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-from ...my_settings import *
+from my_settings import *
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
