c14e0d7acd5e9f4102277d67b7e09df87c7ffe40
melody <myeonghee.son@gmail.com>
2022-02-07 16:47:49 +0900

    ADD:settings Database

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,3 +3,14 @@ ALLOWED_HOSTS = ['52.78.145.43', 'melodyboard.shop']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG=False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'postres',
+        'PASSWORD': '=eKmx$xxymnxxwNCxxx$SX55*RdjKK1G&',
+        'HOST': 'database-2.capxttw9wm6f.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
